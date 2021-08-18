05be1b047fa8f59410aaf94989aa4325ebab9664
MEGHAN KINSHERF <kinsherf@lesley.edu>
2021-08-17 22:49:14 -0400

    added content to post.php

---
+++ post.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+        <?php include 'head.php' ?>
+    </head>
+    <body>
+        <?php include 'header.php' ?>
+        <?php include 'nav.php' ?>
+    <div id="main">
+        <main>
+            <h2>Blueberry Muffins</h2>
+            <div id="briefdescription">
+                <p>These muffins are extra large and yummy with the sugary-cinnamon crumb topping. I usually double the recipe and fill the muffin cups just to the top edge for a wonderful extra-generously-sized deli style muffin. Add extra blueberries too, if you want!</p>
+            </div><!--close for briefdescription-->
+            <div id="author">
+                <p><a href="https://www.allrecipes.com/recipe/6865/to-die-for-blueberry-muffins/">allrecipes</a></p>
+            </div><!--close for author-->
+            <img src="images/blueberrymuffin.jpg" alt="Blueberry Muffins on Cooling Tray" title="Blueberry Muffins">
+            <div id="ingredients">
+                <h3>Ingredients</h3>
+                <ul>
+                    <li>1 1/2 cups all-purpose flour</li>
+                    <li>3/4 cup white sugar</li>
+                    <li>1/2 tsp salt</li>
+                    <li>2 tsp baking powder</li>
+                    <li>1/3 cup vegetable oil</li>
+                    <li>1 egg</li>
+                    <li>1/3 cup milk, or more as needed</li>
+                    <li>1 cup fresh blueberries</li>
+                    <li>1/2 cup white sugar</li>
+                </ul>
+                <br>
+                <h3>Crumble Topping Ingredients</h3>
+                <ul>
+                    <li>1/3 cup all-purpose flour</li>
+                    <li>1/4 cup butter, cubed</li>
+                    <li>1 1/2 tsp ground cinnamon</li>
+                </ul>
+                <br>
+                <h3>Directions</h3>
+                    <ol>
+                        <li>Preheat oven to 400 degrees F (200 degrees C). Grease muffin cups or line with muffin liners.</li>
+                        <li>To Make Crumb Topping: Mix together 1/2 cup sugar, 1/3 cup flour, 1/4 cup butter, and 1 1/2 teaspoons cinnamon. Mix with fork, and sprinkle over muffins before baking.</li>
+                        <li>Combine 1 1/2 cups flour, 3/4 cup sugar, salt and baking powder. Place vegetable oil into a 1 cup measuring cup; add the egg and add enough milk to reach the 1-cup mark. Mix this with flour mixture. Fold in blueberries. Fill muffin cups right to the top, and sprinkle with crumb topping mixture.</li>
+                        <li>Bake for 20 to 25 minutes in the preheated oven, or until done.
+                    </ol>
+            </div><!--close for ingredients-->
+            <div id="nutrion">
+                <h3>Nutrition Facts</h3>
+                <p>383 calories</p>
+                <p>protein 4.3g</p>
+                <p>carbohydrates 56.9g</p>
+                <p>fat 16.1g</p>
+                <p>cholesterol 39.3mg</p> 
+                <p>sodium 321.8mg</p>
+            </div><!--close for nutrition-->
+        </main>
+    </div><!--close for main-->
+    <?php include 'footer.php' ?>
+    </body>
+</html>
