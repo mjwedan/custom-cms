bcb57f9edeacc0a7332c45338f30eefb9bed3336
MEGHAN KINSHERF <kinsherf@lesley.edu>
2021-08-20 12:05:20 -0400

    added dbconnect

---
+++ dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+$dbServer = "localhost";
+$dbUsername = "root";
+$dbPassword = "root";
+$dbName = "custom_cms";
+//Create Connection
+$conn = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
+?>
