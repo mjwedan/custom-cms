ad09226954cf676efe2b1da12b3382a9476d35aa
MEGHAN KINSHERF <kinsherf@lesley.edu>
2021-08-22 13:30:55 -0400

    fixed database form

--- recipe-submission.php
+++ recipe-submission.php
@@ -11,33 +11,21 @@
                 <h2>Submit a New Recipe!</h2>
                 <form class="recipe-submissions-form" action="submit-recipe.php" method="post">
 <label for="title">Name of Recipe</label><br>
-<input type="text" id="title" name="title" placeholder="title"></input><br><br>
+<input type="text" id="title" name="title" placeholder="Title"></input><br><br>
 <label for="author">Author</label><br>
 <input type="text" id="author" name="author" placeholder="Author"></input><br><br>
 <label for="date">Date</label><br>
 <input type="date" id="date" name="date" placeholder="Date"></input><br><br>
 <label for="briefdescription">Brief Description</label><br>
-<input type="text" id="briefdescription" name="briefdescription" placeholder="briefdescription"></input><br><br>
-<label for="image">Upload an Image</label><br>
-<input type="file" id="myFile" name="filename" placeholder="image"></input><br><br>
+<input type="text" id="briefdescription" name="briefdescription" placeholder="Brief Description"></input><br><br>
 <label for="ingredients">Ingredients</label><br>
 <textarea id="ingredients" name="ingredients" rows="4" cols="50"></textarea> <br><br>
 <label for="directions">Directions</label><br>
 <textarea id="directions" name="directions" rows="4" cols="50"></textarea> <br><br>
 <label for="nutrition">Nutrition Facts</label><br>
 <textarea id="nutrition" name="nutrition" rows="4" cols="50"></textarea> 
-
-<div id="editor"></div>
-
-<script src="/ace-builds/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
-<script>
-    var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/eclipse");
-    editor.session.setMode("ace/mode/javascript");
-</script>
 <button type="submit" value="Submit">SUBMIT</button>
 </form>
-
         </main>
     </div><!--close for main-->
     <?php include 'footer.php' ?>
--- submit-recipe.php
+++ submit-recipe.php
@@ -2,39 +2,19 @@
  
  // get the data to insert into the db
  $title = $_POST["title"];
- $briefdescription = $_POST["briefdescription"];
  $author = $_POST["author"];
- $datepublished = $_POST["datepublished"];
- $image = $_POST["image"];
- $ingredients = $_POST["ingredients"]
- $ingredientdetails = $_POST["ingredientdetails"];
+ $date = $_POST["date"];
+ $briefdescription = $_POST["briefdescription"];
+ $ingredients = $_POST["ingredients"];
  $directions = $_POST["directions"];
- $directiondetails = $_POST["directiondetails"];
  $nutrition = $_POST ["nutrition"];
- $nutritiondetails = $_POST["nutritiondetails"];
 
  // insert the data with the sql query
- include_once 'db_connect.php';
- $sql="INSERT INTO posts (title, content, author, date) VALUES ('" .     
-     $title . "','" .  $content . "','" . $author . "','" . $date . "')";
- $result = mysqli_query($conn, $sql);
- 
- // redirect to homepage
- header("Location: index.php");
+ include_once 'dbconnect.php';
+ $sql="INSERT INTO recipes (title, author, date, briefdescription, ingredients, directions, nutrition) VALUES ('" .     
+ $title . "','" .  $author . "','" . $date . "','" . $briefdescription . "','" . $ingredients . "','" . $directions . "','" . $nutrition ."')";
+$result = mysqli_query($conn, $sql);
 
-if(isset($_POST["submit"])){
-    $check = getimagesize($_FILES["image"]["tmp_name"]);
-    if($check !== false){
-        $image = $_FILES['image']['tmp_name'];
-        $imgContent = addslashes(file_get_contents($image));
-//Insert image content into database
-            $insert = $db->query("INSERT into images (image, created) VALUES ('$imgContent', '$dataTime')");
-            if($insert){
-                echo "File uploaded successfully.";
-            }else{
-                echo "File upload failed, please try again.";
-            } 
-        }else{
-            echo "Please select an image file to upload.";
-        }
+// redirect to homepage
+header("Location: index.php");
 ?>
